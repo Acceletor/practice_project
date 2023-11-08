d7d47864a9f39d9d0a9224724ea78454e874ce51
Karintorn Suebthawilkul <karinsueb@gmail.com>
2023-11-08 16:09:09 +0100

    JPG to PNG convertor

---
+++ JPGtoPNGconverter/JPGtoPNGconverter.py
@@ -0,0 +1,32 @@
+import sys
+import os
+from PIL import Image
+
+#python JPGtoPNGconverter.py pokemon/ new/
+
+#grap first and second index from terminal
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+#check is new/ exist, if not create
+
+def convertImg (image_folder,output_folder):
+    if os.path.isdir(output_folder):
+        print((f"{output_folder} folder is really exist"))
+        return
+    else:
+        os.mkdir(output_folder)
+        for filename in os.listdir(image_folder):
+            f = os.path.join(image_folder, filename)
+
+            if os.path.isfile(f):
+                clean_name = os.path.splitext(filename)[0]
+                new_file = f"{output_folder}{clean_name}.png"
+                img = Image.open(f)
+                img.save(new_file,"png")
+
+# loop through pokemon
+# convert images to png
+# save to new folder
+
+convertImg(image_folder,output_folder)
